ff5732e1e76acfe0ebf49db3b1041407b94470a7
Alex Lanzón Horcas <alexlhorcas@gmail.com>
2019-10-13 13:12:37 +0200

    Add files via upload

---
+++ posicion.py
@@ -0,0 +1,7 @@
+#Coding: utf8
+Comunidad = input("De que comunidad viene? ")
+Edad = int(input("Cuantos años tiene? "))
+if Edad >=18 and Comunidad == "murcia":
+    print("A esa edad aún crees que murcia existe? fuera de aqui")
+else:
+    print("Bienvenido tenga un buen dia")
